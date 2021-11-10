8d2a6130b9933d5200232920dc55d3185ebb4708
Mauro Nava Luevanos <mauronavaluevanos63@gmail.com>
2021-11-10 08:26:39 -0600

    Basic scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo'),
+        ),
+        body: const Text(
+          'Hello World',
+        ),
+      ),
     );
   }
 }
